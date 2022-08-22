3b6502754dca19fae94c3b40df35d42e6ca3fec0
douglasahlquist <douglas.ahlquist@gmail.com>
2022-08-21 23:54:57 -0700

    upgrade Java version dependency to 17

--- springboot-oauth2-okta/src/test/java/com/ahlquist/commio/Oauth2GoogleOktaTemplateApplicationTest.java
+++ springboot-oauth2-okta/src/test/java/com/ahlquist/commio/Oauth2GoogleOktaTemplateApplicationTest.java
@@ -1,18 +1,16 @@
 package com.ahlquist.commio;
 
- 
-  import org.junit.jupiter.api.Test; import
-  org.springframework.boot.test.context.SpringBootTest;
-  
-  //first configure the domain in the application.yaml file
-  //then uncomment the testcase
-  
-  //@SpringBootTest (classes=Oauth2GoogleOktaTemplateApplication .class)
-  public class Oauth2GoogleOktaTemplateApplicationTest {
-  
-  //@Test 
-  //public void contextLoads() { }
-  //}
-	  
-  }
- 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//first configure the domain in the application.yaml file
+//then uncomment the testcase
+
+//@SpringBootTest (classes=Oauth2GoogleOktaTemplateApplication .class)
+public class Oauth2GoogleOktaTemplateApplicationTest {
+
+	// @Test
+	// public void contextLoads() { }
+	// }
+
+}
